1b486428fcb1a112bba500e8ec4e610314a88275
Raphael Onofre <raphael.onofre@gmail.com>
2019-04-21 23:38:06 -0300

    Remove old constructor and state creation

--- App.js
+++ App.js
@@ -2,13 +2,6 @@ import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default class App extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state={
-  //     display:'',
-  //     result:''
-  //   }
-  // }
   state = {
     display: "",
     result: ""
